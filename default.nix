210f2ae8dbd6eedf07475036edd3b2df06a866c9
Vadim Makarov <add4che@gmail.com>
2019-11-18 11:52:39 +0300

    add nix-shell for bucklescript

--- default.nix
+++
@@ -1,8 +0,0 @@
-with import <nixpkgs> {};
-
-mkShell {
-  buildInputs = [ nodejs-10_x ] ++ (with ocamlPackages_4_02; [ ocaml ninja merlin ]);
-  shellHook = ''
-    export PATH="`pwd`/node_modules/.bin:$PATH"
-  '';
-}
--- part_1/default.nix
+++ part_1/default.nix
@@ -14,7 +14,7 @@ in
             (python3.withPackages my-python-packages)
         ]; 
         shellHook = ''
-           # fix terminal behavior when running shell with --pure
+            # fix terminal behavior when running shell with --pure
             export TERM=xterm-256color 
         '';
     }
