070550980816c02ac87449d53874b9058210cd48
Vadim Makarov <add4che@gmail.com>
2019-11-20 23:09:42 +0300

    add Concurrency class

---
+++ trik/robot/Concurrency.hx
@@ -0,0 +1,23 @@
+package trik.robot;
+
+class Concurrency {
+    public function joinThread(threadId:String):Void {
+        untyped __js__("Threading.joinThread({0})", threadId);
+    }
+
+    public function killThread(threadId:String):Void {
+        untyped __js__("Threading.killThread({0})", threadId);
+    }
+
+    public function receiveMessage(wait:Bool):String {
+        return untyped __js__("Threading.receiveMessage({0})", wait);
+    }
+
+    public function sendMessage(threadId:String, message:String):Void {
+        return untyped __js__("Threading.sendMessage({0}, {1})", threadId, message);
+    }
+
+    public function startThread(threadId:String, functionName:String):Void {
+        return untyped __js__("Threading.startThread({0}, {1})", threadId, functionName);
+    }
+}
