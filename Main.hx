d97ff908046c36aa8c2cdd3c143b9f20eff3be80
Vadim Makarov <add4che@gmail.com>
2019-11-21 13:22:56 +0300

    add trik.color package

--- Main.hx
+++ Main.hx
@@ -1,14 +1,13 @@
 import trik.robot.motor.Motor;
 import trik.Trik.*;
+import trik.color.Color;
+import trik.color.ColorTools.*;
 import trik.robot.keys.Key;
 
 using Lambda;
 
 class Main {
     public static function main():Void {
-        // var motors = ["M3", "M4"].map(brick.motor);
-        // for (motor in motors)
-        //     motor.setPower(100);
         script.wait(3000);
         print(brick.keys.wasPressed(Left));
     }
