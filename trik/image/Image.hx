cc90881f6f903f36f8e0544bf563b70df13bfc17
Vadim Makarov <add4che@gmail.com>
2019-11-22 21:32:32 +0300

    update Image class

--- trik/image/Image.hx
+++ trik/image/Image.hx
@@ -4,8 +4,24 @@ import trik.color.Color;
 import trik.color.ColorTools.*;
 
 
+@:forward
 abstract Image(Array<Array<Color>>) {
     public function new(pixels:Array<Array<Color>>) {
+        for (i in pixels) 
+            if (i.length != pixels[0].length)
+                throw "all row arrays must have the same length";
+
         this = pixels;
+    }
+
+    @:arrayAccess
+    public inline function get(index:Int):Array<Color> {
+        return this[index];
+    }
+
+    @:arrayAccess
+    public inline function set(index:Int, val:Array<Color>):Array<Color> {
+        this[index] = val;
+        return val;
     }
 }
