2845247d2de8ff92f2c0b9e425cd36a8615cb58d
Vadim Makarov <add4che@gmail.com>
2019-11-26 19:13:10 +0300

    update Artag class

--- trik/artag/Artag.hx
+++ trik/artag/Artag.hx
@@ -1,12 +1,37 @@
 package trik.artag;
 
 import trik.image.Image;
+import trik.image.Corners;
+import trik.image.Sides;
+import trik.image.Pixel;
+import trik.artag.ArtagValues;
 
 using trik.tools.ImageTools;
 
 
-// class Artag {
-//     public function new( ?img:Image ) {
-//         this.
-//     }
-// }
+class Artag {
+    var image:Image;
+
+    function getROI():Image {
+        var tmpImage:Image  = image.erode();
+        var corners:Corners = tmpImage.findCorners();
+        var sides:Sides     = tmpImage.cornersToSides(corners);
+        var roiImage:Image  = tmpImage.cropSides(sides);
+        var width = image[0].length, height = image.length;
+        
+        corners.leftTop.add(new Pixel(-15 + sides.left, -15 + sides.bottom, width, height));
+        corners.rightTop.add(new Pixel(-15 + sides.left, 15 + sides.bottom, width, height));
+        corners.rightBottom.add(new Pixel(15 + sides.left, 15 + sides.bottom, width, height));
+        corners.leftBottom.add(new Pixel(15 + sides.left, -15 + sides.bottom, width, height));
+
+        return image.cropCorners(corners);
+    }
+
+    public function new( ?image:Image ) {
+        this.image = image;
+    }
+
+    // public function read():ArtagValues {
+    //     return new ArtagValues();
+    // }
+}
