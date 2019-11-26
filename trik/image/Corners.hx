fc6095cb51c43a860a538478ab5d7c6e924cd59a
Vadim Makarov <add4che@gmail.com>
2019-11-26 19:03:07 +0300

    turn Corners typedef into a class

--- trik/image/Corners.hx
+++ trik/image/Corners.hx
@@ -3,9 +3,16 @@ package trik.image;
 import trik.image.Pixel;
 
 
-typedef Corners = {
-    leftTop:Null<Pixel>,
-    rightTop:Null<Pixel>,
-    rightBottom:Null<Pixel>,
-    leftBottom:Null<Pixel>
+class Corners {
+    public var leftTop:Null<Pixel>;
+    public var rightTop:Null<Pixel>;
+    public var rightBottom:Null<Pixel>;
+    public var leftBottom:Null<Pixel>;
+
+    public function new(?leftTop:Null<Pixel>, ?rightTop:Null<Pixel>, ?rightBottom:Null<Pixel>, ?leftBottom:Null<Pixel>) {
+        this.leftTop = leftTop;
+        this.rightTop = rightTop;
+        this.rightBottom = rightBottom;
+        this.leftBottom = leftBottom;
+    }
 }
