82c89784c254201d275c322768caaece34ecf8a2
Vadim Makarov <add4che@gmail.com>
2019-11-26 19:13:02 +0300

    add Artag values class

---
+++ trik/artag/ArtagValues.hx
@@ -0,0 +1,13 @@
+package trik.artag;
+
+class ArtagValues {
+    var n:Int;
+    var x:Int; 
+    var y:Int;
+
+    public function new(n:Int, x:Int, y:Int) {
+        this.n = n;
+        this.x = x;
+        this.y = y;
+    }
+}
