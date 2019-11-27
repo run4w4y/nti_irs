82645b43ffc8894353690b513a6df2f5ae50a360
Vadim Makarov <add4che@gmail.com>
2019-11-27 19:17:37 +0300

    fix show_image.py

--- tests/show_image.py
+++ tests/show_image.py
@@ -6,8 +6,8 @@ matplotlib.use('gtk3agg')
 
 with open('image_res.json') as f:
     plt.imshow(json.loads(f.read()), cmap='gray', vmin=0, vmax=255)
-    # plt.plot(123, 127, 'ro')
-    # plt.plot(114, 2, 'ro')
-    # plt.plot(19, 0, 'ro')
-    # plt.plot(1, 127, 'ro')
+    # plt.plot(127, 123, 'ro')
+    # plt.plot(2, 114, 'ro')
+    # plt.plot(0, 19, 'ro')
+    # plt.plot(127, 1, 'ro')
     plt.show()
