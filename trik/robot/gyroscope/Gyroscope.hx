a02ad5d31e80e1886f2afba199b628e54ff771d3
Vadim Makarov <add4che@gmail.com>
2019-11-30 17:52:28 +0300

    add read function to the Gyroscope class

--- trik/robot/gyroscope/Gyroscope.hx
+++ trik/robot/gyroscope/Gyroscope.hx
@@ -4,6 +4,7 @@ extern class Gyroscope {
     public function calibrate            (duration:Int)      :Void;
     public function getCalibrationValues ()                  :Array<Int>; // im not sure what the fuck is this tbh
     public function isCalibrated         ()                  :Bool;
+    public function read                 ()                  :Array<Int>;
     public function readRawData          ()                  :Array<Int>;
     public function setCalibrationValues (values:Array<Int>) :Void;
 }
