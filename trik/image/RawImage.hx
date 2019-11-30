27c510dc2d89a5137179a1c6f30d80f541c53f91
Vadim Makarov <add4che@gmail.com>
2019-11-30 17:54:58 +0300

    update RawImage to the new color scheme

--- trik/image/RawImage.hx
+++ trik/image/RawImage.hx
@@ -1,23 +1,23 @@
 package trik.image;
 
-import trik.color.Color;
-import trik.color.ColorTools.*;
+import trik.color.RGBColor;
+import trik.color.RGB24Color;
+import trik.tools.ColorTools.*;
 import trik.image.Image;
-import Std;
 
 
-abstract RawImage(Array<Color>) {
+abstract RawImage(Array<RGB24Color>) {
     public function new(raw_photo:String):Void {
-        this = [for (i in 0...raw_photo.length) RGB24(Std.parseInt(raw_photo.charAt(i)))];
+        this = [for (i in 0...raw_photo.length) new RGB24Color(Std.parseInt(raw_photo.charAt(i)))];
     }
 
-    public function toImage():Image {
+    public function toImage():Image<RGBColor> {
         var w = 160, h = 120;
-        var res:Array<Array<Color>> = [];
+        var res:Array<Array<RGBColor>> = [];
         for (i in 0...h) {
-            var tmp:Array<Color> = [];
+            var tmp:Array<RGBColor> = [];
             for (j in 0...w)
-                tmp.push(colorToRgb(this[w * i + j]));
+                tmp.push(toRGB(this[w * i + j]));
             res.push(tmp);
         }
         return new Image(res);
