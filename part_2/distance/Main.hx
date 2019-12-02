3c0301adf30f02be912352b18f47a1c1da9638e1
Vadim Makarov <add4che@gmail.com>
2019-12-02 16:28:10 +0300

    add second part solutions

---
+++ part_2/distance/Main.hx
@@ -0,0 +1,20 @@
+import trik.Trik.*;
+import trik.robotModel.Environment;
+
+using Lambda;
+
+
+class Main {
+    public static function main():Void {
+        var model = new FinalModel({
+            leftMotor:    brick.motor("M4"),
+            rightMotor:   brick.motor("M3"),
+            leftEncoder:  brick.encoder("E4"),
+            rightEncoder: brick.encoder("E3"), 
+            frontSensor:  brick.sensor("D1"),
+            leftSensor:   brick.sensor("D2"),
+            environment:  Simulator
+        });
+        model.solution();
+    }
+}
