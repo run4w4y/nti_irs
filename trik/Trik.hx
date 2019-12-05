dfad694ba4a2dcc822a928d0165e56376a5dcda8
Vadim Makarov <add4che@gmail.com>
2019-12-05 19:21:52 +0300

    go back from Printable to Dynamic

--- trik/Trik.hx
+++ trik/Trik.hx
@@ -6,18 +6,13 @@ import trik.robot.Mailbox;
 import trik.robot.Concurrency;
 
 
-typedef Printable = {
-    function toString():String;
-}
-
 class Trik {
     public static var brick = new Brick();
     public static var script = new ScriptHigher();
     public static var mailbox = new Mailbox();
     public static var threading = new Concurrency();
 
-    @:generic
-    public static function print<T:Printable>(obj:T):Void {
+    public static function print(obj:Dynamic):Void {
         untyped __js__("print({0})", Std.string(obj));
     }
 }
