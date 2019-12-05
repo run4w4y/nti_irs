6452f58c018dd61a7c95fa213df2e9f27305d600
Vadim Makarov <add4che@gmail.com>
2019-12-05 16:16:17 +0300

    better constraints for print function

--- trik/Trik.hx
+++ trik/Trik.hx
@@ -6,13 +6,18 @@ import trik.robot.Mailbox;
 import trik.robot.Concurrency;
 
 
+typedef Printable = {
+    public function toString():String;
+}
+
 class Trik {
     public static var brick = new Brick();
     public static var script = new ScriptHigher();
     public static var mailbox = new Mailbox();
     public static var threading = new Concurrency();
 
-    public static function print(obj:Dynamic):Void {
+    @:generic
+    public static function print<T:Printable>(obj:T):Void {
         untyped __js__("print({0})", Std.string(obj));
     }
 }
