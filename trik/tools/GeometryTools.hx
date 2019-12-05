3553583ea2089fa2362c66d6fa2e1ba6282ceca9
Vadim Makarov <add4che@gmail.com>
2019-12-05 16:44:13 +0300

    update GeometryTools to the latest changes

--- trik/tools/GeometryTools.hx
+++ trik/tools/GeometryTools.hx
@@ -1,27 +1,27 @@
 package trik.tools;
 
-import trik.exceptions.SamePointException;
 import trik.geometry.Line;
 import trik.geometry.Point;
+import trik.geometry.Vector;
+import trik.geometry.PointLike;
 import Math.*;
 
 
 class GeometryTools {
-    public static function distToLine(point:Point, line:Line) {
+    @:generic
+    public static function distToLine<T:PointLike>(pointLike:T, line:Line) {
         var d:Float = line.point1.distTo(line.point2);
-        if (d == 0) 
-            throw new SamePointException("cant define a line with two same points");
-        
-        var s:Float = (point.sub(line.point1)).vector_product(point.sub(line.point2));
+        var vector = Vector.fromPointLike(pointLike);
+        var s:Float = (vector.sub(line.point1)).vector_product(vector.sub(line.point2));
         return abs(s)/d;
     }
 
-    public static function det(a:Float, b:Float, c:Float, d:Float):Float {
-        return a * d - b * c;
+    static function det(line1:Line, line2:Line):Float {
+        return line1.a * line2.b - line1.b * line2.a;
     }
 
     public static function getIntersectionPoint(line1:Line, line2:Line):Point {
-        var zn:Float = det(line1.a, line1.b, line2.a, line2.b);
+        var zn:Float = det(line1, line2);
 
         return new Point();
     }
