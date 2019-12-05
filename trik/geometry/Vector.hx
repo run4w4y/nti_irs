e112d2317b4a4b4af3bea58fa8eba1977f72419e
Vadim Makarov <add4che@gmail.com>
2019-12-05 16:44:00 +0300

    add Vector class

--- trik/geometry/Vector.hx
+++ trik/geometry/Vector.hx
@@ -1,21 +1,49 @@
 package trik.geometry;
 
 import trik.geometry.Point;
+import trik.geometry.PointLike;
 
-@:forward
-abstract Vector(Point) {
-    public function new(x: Float, y:Float) {
-        this = new Point(x, y);
+
+class Vector extends Point {
+    public function new(?x:Float=0, ?y:Float=0):Void {
+        super(x, y);
     }
 
-    @:op(A % B)
-    public function scalar_product(point:Point):Float {
-        return this.x * point.x + this.y * point.y;
+    @:generic
+    public static function fromPointLike<T:PointLike>(pointLike:T):Vector {
+        return new Vector(pointLike.x, pointLike.y);
     }
 
-    @:op(A ^ B)
-    public function vector_product(point:Point) {
-        return this.x * point.y - this.y * point.x;
+    @:generic
+    override public function sum<T:PointLike>(pointLike:T):Vector {
+        return new Vector(this.x + pointLike.x, this.y + pointLike.y);
+    }
+
+    override public function neg():Vector {
+        return new Vector(-this.x, -this.y);
+    }
+
+    @:generic
+    override public function sub<T:PointLike>(pointLike:T):Vector {
+        return new Vector(this.x - pointLike.x, this.y - pointLike.y);
+    }
+
+    override public function mul(k:Float):Vector {
+        return new Vector(this.x * k, this.y * k);
+    }
+
+    override public function div(k:Float):Vector {
+        return new Vector(this.x / k, this.y / k);
+    }
+
+    @:generic
+    public function scalar_product<T:PointLike>(pointLike:T):Float {
+        return this.x * pointLike.x + this.y * pointLike.y;
+    }
+
+    @:generic
+    public function vector_product<T:PointLike>(pointLike:T) {
+        return this.x * pointLike.y - this.y * pointLike.x;
     }
 
     public function length():Float {
