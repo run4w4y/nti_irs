57b3c70c43143bb4796b87d2fd2627034386df7f
Vadim Makarov <add4che@gmail.com>
2019-12-18 14:22:20 +0300

    add git to the buildInputs

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { pkgs ? import <nixpkgs> {} }:
 
 pkgs.mkShell {
-    buildInputs = with pkgs; [ haxe ];
+    buildInputs = with pkgs; [ git haxe ];
     shellHook = ''
         # fix terminal behavior when running shell with --pure
         export TERM=xterm-256color
