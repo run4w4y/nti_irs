3f1f20de1d5151965f676861d5a7462bfac354b2
Vadim Makarov <add4che@gmail.com>
2019-12-20 13:36:17 +0300

    update ArtagDecoder

--- src/app/artagDecoder/ArtagDecoder.hx
+++ src/app/artagDecoder/ArtagDecoder.hx
@@ -1,9 +1,37 @@
 package src.app.artagDecoder;
 
 import src.artag.Artag;
+import src.color.BinaryColor;
+import src.range.Range;
 
-class ArtagDecoder extends Artag {
-    public function read() {
+
+abstract ArtagDecoder(Artag) {
+    public function new(artag:Artag):Void {
+        this = artag;
+    }
+
+    function binaryToInt(str:String):Int {
+        var res = 0;
+        var cur = 1;
+
+        for (i in new Range(str.length, 0)) {
+            res += Std.parseInt(str.charAt(i)) * cur;
+            cur *= 2;
+        }
         
+        return res;
+    }
+
+    function colorToDigit(color:BinaryColor):Int {
+        return if (color.value) 1 else 0;
+    }
+
+    public function read():Int {
+        return binaryToInt(
+            Std.string(colorToDigit(this.marker[1][2])) +
+            Std.string(colorToDigit(this.marker[2][1])) +
+            Std.string(colorToDigit(this.marker[2][3])) + 
+            Std.string(colorToDigit(this.marker[3][2]))
+        );
     }
 }
