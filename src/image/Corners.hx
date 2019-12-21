777a738b75b7d40cdd9f59c8e4e5343ab07501b3
Vadim Makarov <add4che@gmail.com>
2019-12-21 23:22:09 +0300

    update types in Corners

--- src/image/Corners.hx
+++ src/image/Corners.hx
@@ -4,12 +4,12 @@ import image.Pixel;
 
 
 class Corners {
-    public var leftTop:Null<Pixel>;
-    public var rightTop:Null<Pixel>;
-    public var rightBottom:Null<Pixel>;
-    public var leftBottom:Null<Pixel>;
+    public var leftTop:Pixel;
+    public var rightTop:Pixel;
+    public var rightBottom:Pixel;
+    public var leftBottom:Pixel;
 
-    public function new(?leftTop:Null<Pixel>, ?rightTop:Null<Pixel>, ?rightBottom:Null<Pixel>, ?leftBottom:Null<Pixel>) {
+    public function new(?leftTop:Pixel, ?rightTop:Pixel, ?rightBottom:Pixel, ?leftBottom:Pixel) {
         this.leftTop = leftTop;
         this.rightTop = rightTop;
         this.rightBottom = rightBottom;
