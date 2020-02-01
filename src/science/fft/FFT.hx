12f4f3fec092d3ab971feb261880951246389178
Vadim Makarov <add4che@gmail.com>
2020-02-01 11:59:36 +0300

    add science.fft

---
+++ src/science/fft/FFT.hx
@@ -0,0 +1,35 @@
+package science.fft;
+
+import science.complex.Complex;
+import exceptions.ValueException;
+
+
+class FFT {
+    public static function fft(amplitudes:Array<Complex>):Array<Complex> {
+        var n = amplitudes.length;
+
+        if (n <= 1) return amplitudes;
+        if (n % 2 != 0) throw new ValueException(
+            'expected length of input array to be a power of 2, got $n'
+        );
+
+        var hn = cast(n/2, Int);
+        var even = fft([for (i in 0...hn) amplitudes[i*2]]);
+        var odd = fft([for (i in 0...hn) amplitudes[2*i + 1]]);
+
+        var res = [for (_ in 0...n) new Complex(0, 0)];
+        for (i in 0...hn) {
+            var ith = -2 * i * Math.PI / n;
+            var wk = new Complex(Math.cos(ith), Math.sin(ith));
+            res[i] = even[i] + wk * odd[i];
+            res[i + hn] = even[i] - wk * odd[i];
+        }
+        return res;
+    }
+
+
+    public static function ifft(amplitudes:Array<Complex>):Array<Complex> {
+        var n = amplitudes.length;
+        return [for (j in fft([for (i in 0...n) amplitudes[i].conjugate()])) j.conjugate() / n];
+    }
+}
