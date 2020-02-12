662e17c77b9c1cf3a29df0daf9c2de0f35541d04
Vadim Makarov <add4che@gmail.com>
2020-02-12 09:48:26 +0300

    move Matrix and make it into an abstract

--- src/science/Matrix.hx
+++
@@ -1,3 +0,0 @@
-package science;
-
-typedef Matrix<T> = Array<Array<T>>;
---
+++ src/science/matrix/Matrix.hx
@@ -0,0 +1,33 @@
+package science.matrix;
+
+using tools.NullTools;
+
+
+@:forward
+abstract Matrix<T>(Array<Array<T>>) from Array<Array<T>> to Array<Array<T>> {
+    public var width(get, never):Int;
+    public var height(get, never):Int;
+
+    private function get_width():Int {
+        if (height == 0)
+            return 0;
+
+        return this[0].length;
+    }
+
+    private function get_height():Int {
+        return this.length;
+    }
+
+    public function new(?a:Array<Array<T>>) {
+        this = a.coalesce([]);
+    }
+
+    @:op([]) public function getIndex(i:Int):Array<T> {
+        return this[i];
+    }
+
+    @:op([]) public function setIndex(i:Int, value:Array<T>):Array<T> {
+        return this[i] = value;
+    }
+}
