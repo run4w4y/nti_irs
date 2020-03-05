bcd4e74de57e22572165fa3ec6d57cfa36f4775b
Vadim Makarov <add4che@gmail.com>
2020-03-06 00:49:17 +0300

    add docs for HexColor

--- src/color/HexColor.hx
+++ src/color/HexColor.hx
@@ -6,7 +6,13 @@ import exceptions.ValueException;
 using tools.NullTools;
 
 
+/**
+    Class used to represent hexdec colors.
+**/
 class HexColor extends BaseColor {
+    /**
+        Hexdec string of the color.
+    **/
     public var value:String;
 
     function hexToInt(str:String) {
@@ -21,6 +27,11 @@ class HexColor extends BaseColor {
         });
     }
 
+    /**
+        Class constructor.
+
+        @param hex hexdec color, for example: 000000, 333333, FFFFFF
+    **/
     public function new(hex:String) {
         if (hex.length != 6)
             throw new ValueException("hex string must be of length 6");
