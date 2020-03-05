ca2cee1709bbac18d11d904fc5ba29c52f189194
Vadim Makarov <add4che@gmail.com>
2020-03-06 00:37:35 +0300

    add docs for Color

--- src/color/Color.hx
+++ src/color/Color.hx
@@ -1,5 +1,8 @@
 package color;
 
+/**
+    Interface for colors. All the colors must have R, G and B fields. 
+**/
 interface Color {
     public var r(get, null):Int;
     public var g(get, null):Int;
