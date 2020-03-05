2c3951c17324b44475b8d3da37974ebab1a3a22e
Vadim Makarov <add4che@gmail.com>
2020-03-06 00:31:33 +0300

    add docs for BaseColor

--- src/color/BaseColor.hx
+++ src/color/BaseColor.hx
@@ -3,10 +3,22 @@ package color;
 import color.Color;
 
 
+/**
+    Class used as the base for all the other colors. 
+**/
 class BaseColor implements Color {
     var rgbValue:Null<RGB> = null;
+    /**
+        Calculates (if haven't yet been calculated) and returns the R value of the RGB representation of this color.
+    **/
     @:isVar public var r(dynamic, null):Int;
+    /**
+        Calculates (if haven't yet been calculated) and returns the G value of the RGB representation of this color.
+    **/
     @:isVar public var g(dynamic, null):Int;
+    /**
+        Calculates (if haven't yet been calculated) and returns the B value of the RGB representation of this color.
+    **/
     @:isVar public var b(dynamic, null):Int;
 
     function calculateRGB():Void {};
