30895287325e2c7f53ad1037f31049918dc251f1
Vadim Makarov <add4che@gmail.com>
2020-03-07 17:18:50 +0300

    update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,20 +1,6 @@
-{ pkgsPath ? <nixpkgs> }:
+{ pkgs ? import ./pkgs.nix {} }:
 
-with rec { 
-    haxe4nix = 
-        (import pkgsPath {}).fetchFromGitHub {
-            owner = "run4w4y";
-            repo = "haxe4nix";
-            rev = "657b8eb6bd6c783d25956c47957007a5cbc7d38b";
-            sha256 = "14mgqrjknihw3qygmkn8n0kyh58qpgarfdjx6silk4ywz0jm6x7a";
-        };
-    pkgsOverlay = 
-        import "${haxe4nix}/nixpkgs-overlay";
-    pkgs = 
-        import pkgsPath { overlays = [ pkgsOverlay ]; };
-};
 with pkgs;
-
 mkShell {
     buildInputs = 
         [ 
