72e15afc029745764b0a155536d26991a4b8327f
Vadim Makarov <add4che@gmail.com>
2020-03-07 18:00:47 +0300

    update nix-shell

--- shell.nix
+++ shell.nix
@@ -1,5 +1,9 @@
 { pkgs ? import ./pkgs.nix {} }:
 
+let 
+    haxePackages = import ./haxeDeps.nix; 
+in
+
 with pkgs;
 mkShell {
     buildInputs = 
@@ -12,5 +16,12 @@ mkShell {
         ''
             # fix terminal behavior when running shell with --pure
             export TERM=xterm-256color
+            # patch haxe up
+            export HOME="$(pwd)"
+            echo "$(pwd)" > .haxelib
+            # set up haxelib and deps
+            haxelib --quiet setup ./lib 
+            haxelib --quiet dev polygonal-ds ${haxePackages.polygonal-ds}
+            haxelib --quiet dev utest ${haxePackages.utest}
         '';
 }
