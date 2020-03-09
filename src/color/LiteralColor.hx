ebbb780da8c20ce295263ad910eccd73fa313190
Vadim Makarov <add4che@gmail.com>
2020-03-09 14:10:07 +0300

    add docs for LiteralColor

--- src/color/LiteralColor.hx
+++ src/color/LiteralColor.hx
@@ -3,9 +3,20 @@ package color;
 import color.RGBColor;
 
 
+/**
+    Class for assigning some name to an RGB color.
+**/
 class LiteralColor extends RGBColor {
     public var name:String;
 
+    /**
+        Class constructor.
+
+        @param r R value of the color
+        @param g G value of the color
+        @param b B value of the color
+        @param name name of the color
+    **/
     public function new(r:Int, g:Int, b:Int, name:String):Void {
         super(r, g, b);
         this.name = name;
