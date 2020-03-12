3242d9b9d9f23963f500d937464624e1cfbb577d
Vadim Makarov <add4che@gmail.com>
2020-03-12 19:48:38 +0300

    remove ArtagDecoderTestCase

--- src/app/tests/Main.hx
+++ src/app/tests/Main.hx
@@ -3,7 +3,6 @@ package app.tests;
 import utest.Runner;
 import utest.ui.Report;
 import app.tests.artag.ArtagTestCase;
-import app.tests.artagDecoder.ArtagDecoderTestCase;
 import app.tests.scientificTools.ScientificToolsTestCase;
 
 
@@ -11,7 +10,6 @@ class Main {
     public static function main() {
         var runner = new Runner();
         runner.addCase(new ArtagTestCase('tests/artagTests'));
-        runner.addCase(new ArtagDecoderTestCase('tests/artagDecoderTests'));
         runner.addCase(new ScientificToolsTestCase('tests/scientificToolsTests'));
         Report.create(runner);
         runner.run();
