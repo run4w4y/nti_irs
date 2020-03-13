0d50f1e353c987f02dffceda4bcb324c99ad592a
Vadim Makarov <add4che@gmail.com>
2020-03-13 14:37:40 +0300

    add solution for the latest online task

--- src/app/main/Main.hx
+++ src/app/main/Main.hx
@@ -11,17 +11,18 @@ import image.Pixel;
 
 class Main {
     public static function main():Void {
-        var img = new RawImage(Script.readAll('input.txt')[0]).toImage();
-        var artag = new Artag(img);
-        var values = new ArtagDecoder(artag).read();
-        Brick.display.addLabel(
-            values.map(function (a) return switch(a) {
-                case Go:        'F';
-                case TurnRight: 'R';
-                case TurnLeft:  'L';
-                case _:         'N';
-            }).join(' '),
-            new Pixel(0, 0)
-        );
+        var img = new RawImage(Script.readAll('input.txt')[0], Hex).toImage();
+        var artag = new Artag(img, true, 8);
+        var decoder = new ArtagDecoder(artag);
+        Script.print(decoder.readRaw());
+        var values = decoder.read();
+        var ans = values.map(function (a) return switch(a) {
+            case Go:        '3';
+            case TurnRight: '2';
+            case TurnLeft:  '1';
+            case _:         '0';
+        }).join(' ');
+        Brick.display.addLabel(ans, new Pixel(0, 0));
+        Script.print(ans);
     }
 }
