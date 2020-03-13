4b37bca11b9dcdac43408cf18672dc3101d22569
Vadim Makarov <add4che@gmail.com>
2020-03-13 13:52:37 +0300

    update RawImage into a class

--- src/image/RawImage.hx
+++ src/image/RawImage.hx
@@ -2,24 +2,50 @@ package image;
 
 import color.RGBColor;
 import color.RGB24Color;
-import tools.ColorTools.*;
+import color.HexColor;
 import image.Image;
 
+using StringTools;
+using tools.NullTools;
+using tools.ColorTools;
 
-abstract RawImage(Array<RGB24Color>) {
-    public function new(rawPhoto:String):Void {
-        this = [for (i in 0...rawPhoto.length) new RGB24Color(Std.parseInt(rawPhoto.charAt(i)))];
+
+enum RawColorScheme {
+    Rgb24;
+    Hex;
+}
+
+
+class RawImage {
+    var photoString:String;
+    var w:Int;
+    var h:Int;
+    var colorScheme:RawColorScheme;
+
+    public function new(rawString, ?colorScheme:RawColorScheme, ?w:Int = 160, ?h:Int = 120):Void {
+        photoString = rawString;
+        this.w = w;
+        this.h = h;
+        this.colorScheme = colorScheme.coalesce(Rgb24);
     }
 
     public function toImage():Image<RGBColor> {
-        var w = 160, h = 120;
+        var raw:Array<RGBColor> = switch (colorScheme) {
+            case Rgb24:
+                [for (i in 0...photoString.length) new RGB24Color(Std.parseInt(photoString.charAt(i))).toRGB()];
+            case Hex:
+                photoString.trim().split(' ').map(function (a) return new HexColor(a).toRGB());
+        };
+
         var res:Array<Array<RGBColor>> = [];
+
         for (i in 0...h) {
             var tmp:Array<RGBColor> = [];
             for (j in 0...w)
-                tmp.push(toRGB(this[w * i + j]));
+                tmp.push(raw[w * i + j]);
             res.push(tmp);
         }
+        
         return new Image(res);
     }
 }
