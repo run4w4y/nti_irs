1cc6913b1cfdfd78a8ed836041d4d38d312e8c1f
Vadim Makarov <add4che@gmail.com>
2020-03-14 14:00:45 +0300

    update trik.Mailbox to use PoolMember

--- src/trik/Mailbox.hx
+++ src/trik/Mailbox.hx
@@ -1,11 +1,18 @@
 package trik;
 
+import connectionPool.PoolMember;
+
+
 class Mailbox {
-    public static function connect(ip:String, ?port:Int=-1):Void {
-        if (port == -1) 
+    public static function connect_(ip:String, ?port:Int):Void {
+        if (port == null) 
             untyped __js__("mailbox.connect({0})", ip);
         else
             untyped __js__("mailbox.connect({0}, {1})", ip, port);
+    }
+
+    public static function connect(member:PoolMember):Void {
+        connect_(member.ip, member.port);
     }
 
     public static function hasMessages():Bool {
@@ -20,10 +27,10 @@ class Mailbox {
         return untyped __js__("mailbox.receive()");
     }
 
-    public static function send(message:String, ?robotNumber:Int=-1):Void {
-        if (robotNumber == -1)
+    public static function send(message:String, ?member:PoolMember):Void {
+        if (member == null)
             untyped __js__("mailbox.send({0})", message);
         else
-            untyped __js__("mailbox.send({0}, {1})", robotNumber, message);
+            untyped __js__("mailbox.send({0}, {1})", member.id, message);
     }
 }
