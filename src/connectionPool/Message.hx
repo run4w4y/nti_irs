cce0ce882a7aac90f68f9e948037ca01342ae77e
Vadim Makarov <add4che@gmail.com>
2020-03-14 14:34:49 +0300

    make receiver optional in Message

--- src/connectionPool/Message.hx
+++ src/connectionPool/Message.hx
@@ -9,9 +9,9 @@ import connectionPool.PoolMember;
 class Message {
     public var object:Dynamic;
     public var sender:PoolMember;
-    public var receiver:PoolMember;
+    public var receiver:Null<PoolMember>;
 
-    public function new(sender:PoolMember, receiver:PoolMember, object:Dynamic) {
+    public function new(object:Dynamic, sender:PoolMember, ?receiver:PoolMember) {
         this.object = object;
         this.sender = sender;
         this.receiver = receiver;
