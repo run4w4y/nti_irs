0f0aa0349f51748b5b08aee55e4bf614515d62b2
Vadim Makarov <add4che@gmail.com>
2020-03-15 03:39:24 +0300

    update Messenger interface to init ChildMessengers

--- src/connectionPool/message/Messenger.hx
+++ src/connectionPool/message/Messenger.hx
@@ -2,6 +2,7 @@ package connectionPool.message;
 
 import connectionPool.PoolMember;
 import connectionPool.message.Message;
+import connectionPool.message.ChildMessenger;
 import connectionPool.request.RequestForm;
 
 
@@ -11,4 +12,5 @@ interface Messenger {
     public function response(request:Message):Void;
     public function waitForMessage():Message;
     public function waitForResponse():Message;
+    public function initChildMessenger(child:ChildMessenger):Void;
 }
