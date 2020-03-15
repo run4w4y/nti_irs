5e404c727cdea38465186bf8ab8a4b5a20abd25e
Vadim Makarov <add4che@gmail.com>
2020-03-15 03:40:17 +0300

    update PoolAction to use ChildMessenger instead

--- src/connectionPool/action/PoolAction.hx
+++ src/connectionPool/action/PoolAction.hx
@@ -1,22 +1,22 @@
 package connectionPool.action;
 
-import ds.HashMap;
 import connectionPool.message.Message;
-import connectionPool.message.Messenger;
-import connectionPool.message.Receiver;
-import connectionPool.request.RequestForm;
-import connectionPool.request.RequestHandler;
+import connectionPool.message.ChildMessenger;
 import connectionPool.action.ActionState;
 import connectionPool.PoolMember;
+import connectionPool.message.Types;
 
 
-class PoolAction implements Messenger {
+class PoolAction implements ChildMessenger {
     static var newId = 0;
     public var state:ActionState;
     public var agent:PoolMember;
     public var id:Int;
-    public var handlers = new HashMap<Int, RequestHandler>();
-    public var members = new HashMap<Int, PoolMember>();
+    public var send:SendFunction;
+    public var request:RequestFunction;
+    public var response:ResponseFunction;
+    public var waitForMessage:WaitFunction;
+    public var waitForResponse:WaitFunction;
 
     public function new(action:Void -> Void, agent:PoolMember) {
         state = NotStarted;
@@ -25,49 +25,11 @@ class PoolAction implements Messenger {
         ++newId;
     }
 
-    public function addHandlers(handlers:Array<RequestHandler>):Void {
-        for (handler in handlers) 
-            this.handlers[handler.id] = handler;
-    }
-
-    public function addMembers(members:Array<PoolMember>):Void {
-        for (member in members)
-            this.members[member.id] = member;
-    }
-
     function executeInner():Void {}
 
     public function execute():Void {
         state = Pending;
         executeInner();
         state = Finished;
-    }
-
-    public function waitForMessage():Message {
-        return Receiver.waitForMessage(response);
-    }
-
-    public function waitForResponse():Message {
-        return Receiver.waitForResponse(response);
-    }
-
-    public function send(object:Dynamic, ?receiver:PoolMember) {
-        new Message(object, 'Info', agent, receiver).send();
-    }
-
-    public function request(form:RequestForm, receiver:PoolMember):Dynamic {
-        var rawForm = {handlerId: form.handler.id, params: form.params};
-        new Message(rawForm, 'Request', agent, receiver).send();
-        return waitForResponse();
-    }
-
-    // TODO: error handling when RequestHandler was not found
-    public function response(request:Message) {
-        new Message(
-            handlers[request.object.handlerId].call(request.object.params), 
-            'Response', 
-            agent, 
-            request.sender
-        ).send();
     }
 }
