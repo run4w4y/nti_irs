28bcd2bcb75d7ef7026a3c6a5f8f95c041039297
Vadim Makarov <add4che@gmail.com>
2020-03-15 23:51:38 +0300

    make ip optional in PoolMember

--- src/connectionPool/PoolMember.hx
+++ src/connectionPool/PoolMember.hx
@@ -1,11 +1,11 @@
 package connectionPool;
 
 class PoolMember {
-    public var ip:String;
+    public var ip:Null<String>;
     public var port:Null<Int>;
     public var id:Int;
 
-    public function new(id:Int, ip:String, ?port:Int) {
+    public function new(id:Int, ?ip:String, ?port:Int) {
         this.ip = ip;
         this.port = port;
         this.id = id;
