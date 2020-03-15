12e16533220800251fb1cca1acc4c999fc8f89d1
Vadim Makarov <add4che@gmail.com>
2020-03-15 23:51:01 +0300

    update types in Gyroscope

--- src/trik/robot/gyroscope/Gyroscope.hx
+++ src/trik/robot/gyroscope/Gyroscope.hx
@@ -1,10 +1,10 @@
 package trik.robot.gyroscope;
 
 extern class Gyroscope {
-    public function calibrate            (duration:Int)      :Void;
-    public function getCalibrationValues ()                  :Array<Int>; // im not sure what the fuck is this tbh
-    public function isCalibrated         ()                  :Bool;
-    public function read                 ()                  :Array<Int>;
-    public function readRawData          ()                  :Array<Int>;
-    public function setCalibrationValues (values:Array<Int>) :Void;
+    public function calibrate            (duration:Int)   :Void;
+    public function getCalibrationValues ()               :Dynamic;
+    public function isCalibrated         ()               :Bool;
+    public function read                 ()               :Array<Int>;
+    public function readRawData          ()               :Array<Int>;
+    public function setCalibrationValues (values:Dynamic) :Void;
 }
