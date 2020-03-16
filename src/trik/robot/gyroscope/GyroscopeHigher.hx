4bb4af99a8610bb714fb2921a3201402982c9d40
Vadim Makarov <add4che@gmail.com>
2020-03-16 13:51:40 +0300

    fix read method in GyroscopeHigher

--- src/trik/robot/gyroscope/GyroscopeHigher.hx
+++ src/trik/robot/gyroscope/GyroscopeHigher.hx
@@ -14,7 +14,7 @@ abstract GyroscopeHigher(Gyroscope) {
     }
 
     public function read():Angle {
-        return new Angle(this.read()[6]/1000 - 360);
+        return new Angle(360 + this.read()[6]/1000);
     }
 
     public function calibrate(duration:Time):Void {
