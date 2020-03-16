37c23a212231738c60db1029dc3ca889ae4d282d
Vadim Makarov <add4che@gmail.com>
2020-03-16 13:52:50 +0300

    update RealManager

--- src/robotModel/motorManager/RealManager.hx
+++ src/robotModel/motorManager/RealManager.hx
@@ -10,38 +10,23 @@ import robotModel.speedManager.pid.PID;
 
 class RealManager extends BaseManager implements MotorManager {
     public function turn(angle:Float):Void {
-        resetEncoders();
         currentDirection += angle;
 
-        var v = 35;
-        var kLeft = if (angle < 0) -1 else 1;
-        var kRight = if (angle < 0) 1 else -1;
-        var pid = new PID(Seconds(.01), -100, 100, {
-            kp: .8,
-            kd: .6,
-            ki: .002
+        var pid = new PID(Seconds(.05), -50, 50, {
+            kp: 17.5,
+            kd: 12.3
         });
-
-        while (abs(Brick.gyroscope.read() - currentDirection) > 0.75) {
-            var u = pid.calculate(readRight() + readLeft());
-            resetEncoders();
-            Script.print('
-                Left: ${round((v + u) * kLeft)}, u: $u
-                Right: ${round((v - u) * kRight)}, u: ${-u}
-                Gyro: ${currentDirection - Brick.gyroscope.read()}
-            ');
-
-            if (angle < 0) {
-                startLeft(round((v + u) * kLeft));
-                startRight(round((v - u) * kRight));
-            } else {
-                startLeft(round((v - u) * kLeft));
-                startRight(round((v + u) * kRight));
-            }
-
-            Script.wait(Seconds(.01));
-        }
-
+        move(
+            0, 
+            pid,
+            function () 
+                return Brick.gyroscope.read() - currentDirection,
+            function() {
+                var error = Brick.gyroscope.read() - currentDirection;
+                return abs(error) > 0.1;
+            }, 
+            Seconds(.05)
+        );
         stop(Seconds(0.1));
     }
 
