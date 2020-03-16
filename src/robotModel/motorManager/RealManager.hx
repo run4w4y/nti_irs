eabe885847b45cb89fc9c898827726fdf3fadc67
Vadim Makarov <add4che@gmail.com>
2020-03-16 18:21:16 +0300

    add sensors and acceleration to RealManager

--- src/robotModel/motorManager/RealManager.hx
+++ src/robotModel/motorManager/RealManager.hx
@@ -3,9 +3,11 @@ package robotModel.motorManager;
 import Math.*;
 import trik.Brick;
 import trik.Script;
+import trik.robot.sensor.Sensor;
 import robotModel.motorManager.BaseManager;
 import robotModel.motorManager.MotorManager;
 import robotModel.speedManager.pid.PID;
+import robotModel.speedManager.SineAcceleration;
 
 
 class RealManager extends BaseManager implements MotorManager {
@@ -42,14 +44,35 @@ class RealManager extends BaseManager implements MotorManager {
         turn(180);
     }
 
-    public function goEncoders(encValue:Int, ?acceleration = false):Void {
-        moveGyro(90, function () {
-            return (readLeft() + readRight()) / 2 <= encValue;
-        });
+    public function goEncoders(encValue:Int, ?accelPoint:Int, ?decelPoint:Int):Void {
+        var accel = new SineAcceleration(35, 90, accelPoint, decelPoint, encValue);
+        resetEncoders();
+
+        var rightVal:Int, leftVal:Int, curPath:Float;
+        do {
+            rightVal = readRight();
+            leftVal = readLeft();
+            curPath = (leftVal + rightVal) / 2;
+            var v = accel.calculate(curPath);
+            moveGyro(round(v));
+            Script.wait(Seconds(.05));
+        } while (curPath <= encValue);
         stop(Seconds(0.1));
     }
 
     public inline function goMillimeters(length:Int, ?acceleratiion = false):Void {
         goEncoders(round(mmToEnc(length)));
+    }
+
+    function checkSensor(sensor:Sensor):Bool {
+        return sensor.read() <= 15;
+    }
+
+    inline function checkLeft():Bool {
+        return checkSensor(leftSensor);
+    }
+
+    inline function checkRight():Bool {
+        return checkSensor(rightSensor);
     }
 }
