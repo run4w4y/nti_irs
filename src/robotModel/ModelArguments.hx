e01586f11b532375c076de1e1d667c77513deb59
Vadim Makarov <add4che@gmail.com>
2020-03-16 03:09:46 +0300

    update ModelArguments

--- src/robotModel/ModelArguments.hx
+++ src/robotModel/ModelArguments.hx
@@ -7,11 +7,11 @@ import robotModel.Environment;
 
 
 typedef ModelArguments = {
-    leftMotor:Motor,
-    rightMotor:Motor,
-    leftEncoder:Encoder,
-    rightEncoder:Encoder,
+    frontSensor:Sensor,
+    leftSensor:Sensor,
+    rightSensor:Sensor,
+    backSensor:Sensor,
+    cellSize:Float,
     environment:Environment,
-    wheelRadius:Float,
     ?cameraPort:String
 }
