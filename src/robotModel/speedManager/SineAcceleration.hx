b816bbb689bc4aba8546351206e4307710a85adf
Vadim Makarov <add4che@gmail.com>
2020-03-18 00:06:20 +0300

    SineAcceleration bug fixes

--- src/robotModel/speedManager/SineAcceleration.hx
+++ src/robotModel/speedManager/SineAcceleration.hx
@@ -14,17 +14,17 @@ class SineAcceleration implements SpeedManager {
     var minSpeed:Int;
 
     public function new(minSpeed:Int, maxSpeed:Int, ?accelPoint:Int, ?decelPoint:Int, ?path:Int) {
+        this.minSpeed = minSpeed;
+        this.maxSpeed = maxSpeed;
+        this.accelPoint = accelPoint;
+        this.decelPoint = path - decelPoint;
+
         if (decelPoint - accelPoint < 0)
             throw new ValueException('accelPoint and decelPoint cant overlap with each other');
         if (decelPoint > path)
             throw new ValueException('decelPoint must not be bigger than path');
         if (decelPoint != null && path == null)
             throw new ValueException('path must not be null if decelPoint was defined');
-
-        this.minSpeed = minSpeed;
-        this.maxSpeed = maxSpeed;
-        this.accelPoint = accelPoint;
-        this.decelPoint = decelPoint;
 
         accelF = 
             if (accelPoint != null) 
@@ -35,7 +35,7 @@ class SineAcceleration implements SpeedManager {
         decelF = 
             if (decelPoint != null)
                 function (x:Float)
-                    return sin((path - x) * (PI / 2 / (path - decelPoint)))
+                    return sin(path - x) * (PI / 2 / this.decelPoint)
             else
                 null;
     }
