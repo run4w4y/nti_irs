6fa3a6d33cb9e2d0a8d4eb3d77a92264b55a006d
Vadim Makarov <add4che@gmail.com>
2020-03-17 16:23:42 +0300

    add PIDSim

---
+++ src/robotModel/speedManager/pid/PIDSim.hx
@@ -0,0 +1,58 @@
+package robotModel.speedManager.pid;
+
+import robotModel.speedManager.SpeedManager;
+import time.Time;
+import robotModel.speedManager.pid.PIDCoefficients;
+import Math.*;
+
+using tools.TimeTools;
+using tools.NullTools;
+
+
+class PIDSim implements SpeedManager {
+    var interval  :Time;
+    var min       :Float;
+    var max       :Float;
+    var kp        :Float;
+    var kd        :Float;
+    var ki        :Float;
+    var prevError :Float = 0;
+    var integral  :Float = 0;
+
+    public function new(interval:Time, min:Float, max:Float, ks:PIDCoefficients) {
+        this.interval = interval;
+        this.min = min;
+        this.max = max;
+        this.kp = ks.kp;
+        this.kd = ks.kd.coalesce(0);
+        this.ki = ks.ki.coalesce(0);
+    }
+
+    public function clear() {
+        prevError = 0;
+        integral = 0;
+    }
+
+    public function calculate(error:Float):Float {
+        var pOut:Float = error * kp;
+        
+        var derivative:Float;
+        switch (interval.toMilliseconds()) {
+            case Milliseconds(timeValue):
+                integral += error * timeValue;
+                derivative = (prevError * error) / timeValue;
+            case _:
+                return 0;
+        }
+        
+        var iOut:Float = integral * ki;
+        var dOut:Float = derivative * kd;
+
+        var res:Float = pOut + iOut + dOut;
+        if (res > max) res = max;
+        if (res < min) res = min;
+
+        prevError = error;
+        return res;
+    }
+}
