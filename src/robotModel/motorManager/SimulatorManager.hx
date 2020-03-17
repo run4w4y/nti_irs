0370256e9d1b0bd4866ffba6d1bf3f3ea34c0ffa
Vadim Makarov <add4che@gmail.com>
2020-03-17 16:25:30 +0300

    use PIDSim in SimulatorManager

--- src/robotModel/motorManager/SimulatorManager.hx
+++ src/robotModel/motorManager/SimulatorManager.hx
@@ -3,11 +3,22 @@ package robotModel.motorManager;
 import Math.*;
 import trik.Brick;
 import trik.Script;
+import time.Time;
+import robotModel.speedManager.pid.PIDSim;
+import robotModel.speedManager.pid.PIDCoefficients;
 import robotModel.motorManager.BaseManager;
 import robotModel.motorManager.MotorManager;
 
+using tool.NullTools;
+
 
 class SimulatorManager extends BaseManager implements MotorManager {
+    static var gyroPID = new PIDSim(Seconds(.01), -100, 100, {
+        kp: 1.05,
+        kd: 0.4,
+        ki: 0.0001
+    });
+
     public function turn(angle:Float):Void {
         currentDirection += angle;
         moveGyro(0, function() {
@@ -29,7 +40,7 @@ class SimulatorManager extends BaseManager implements MotorManager {
         turn(180);
     }
 
-    public function goEncoders(encValue:Int, ?acceleration = false):Void {
+    public function goEncoders(encValue:Int, ?_ :Int, ?_:Int):Void {
         moveGyro(90, function () {
             return (readLeft() + readRight()) / 2 <= encValue;
         });
@@ -38,5 +49,20 @@ class SimulatorManager extends BaseManager implements MotorManager {
 
     public inline function goMillimeters(length:Int, ?acceleratiion = false):Void {
         goEncoders(round(mmToEnc(length)));
+    }
+
+    override function moveGyro(speed:Int, ?condition:(Void -> Bool), ?interval:Time, ?coefficients:PIDCoefficients):Void {
+        var defaults:PIDCoefficients = {
+            kp: 1.05,
+            kd: 0.4,
+            ki: 0.0001
+        };
+        if (condition != null)
+            gyroPID = new PIDSim(interval.coalesce(Seconds(.01)), -100, 100, coefficients.coalesce(defaults));
+        move(speed, gyroPID, function() {
+                return Brick.gyroscope.read() - currentDirection;
+            }, 
+            condition, interval
+        );
     }
 }
