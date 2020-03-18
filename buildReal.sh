526bab2a84912d97cc1b389db9b1b8920bbc1e30
Vadim Makarov <add4che@gmail.com>
2020-03-19 00:47:24 +0300

    add buildReal.sh

---
+++ buildReal.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+buildHaxe() {
+    echo "Building $1""_$3.js with main $5"
+    local base64Input=$(echo "$2" | base64)
+    haxe \
+        --macro "addGlobalMetadata('', '@:build(Build.buildReal())')" \
+        --macro "addGlobalMetadata('', '@:build(Build.build())')" \
+        -cp src \
+        -D js-es=3 \
+        -D "$1=$2" \
+        -js "builds/out/$4/$1""_$3.js" \
+        -main $5 \
+        -lib polygonal-ds
+}
+
+mkdir -p "builds/out/$1"
+buildInputPath="builds/in/$1.in"
+
+curName=
+curTest=''
+curMain=
+testCount=0
+
+while IFS= read line; do
+    case "$line" in
+        *:*)
+            ARR=(${line//:/ })
+            curName=${ARR[0]}
+            curMain=${ARR[1]}
+            testCount=0
+            ;;
+        ---*)
+            testCount=$(( $testCount + 1 ))
+            buildHaxe "$curName" "$curTest" $testCount "$1" $curMain
+            curTest=
+            ;;
+        ===*)
+            testCount=$(( $testCount + 1 ))
+            buildHaxe "$curName" "$curTest" $testCount "$1" $curMain
+            curName=
+            curTest=
+            curMain=
+            testCount=0
+            ;;
+        *)
+            if [ -z "$curTest" ]; then
+                curTest="$line"
+            else
+                curTest=$(printf "$curTest\n$line")
+            fi
+            ;;
+    esac
+done <<< $(cat "$buildInputPath")\=
