3d3429a07a74da2ba15c571c196f563847645140
Vadim Makarov <add4che@gmail.com>
2020-03-18 10:04:29 +0300

    add solution for sim1 of the first day

--- src/robotModel/RobotModel.hx
+++ src/robotModel/RobotModel.hx
@@ -7,8 +7,11 @@ import trik.robot.sensor.Sensor;
 import robotModel.Environment;
 import robotModel.ModelArguments;
 import robotModel.motorManager.MotorManager;
+import movementExecutor.MovementExecutor;
+import movementExecutor.Movement;
 
 using tools.NullTools;
+using StringTools;
 
 
 class RobotModel {
@@ -52,7 +55,33 @@ class RobotModel {
     }
 
     public function solution():Void {
-        manager.goEncoders(2000, 360, 360);
+        if (environment == Simulator)
+            manager.goEncoders(150);
+        var executor = 
+            if (environment == Simulator)
+                new MovementExecutor(manager, 1370)
+            else
+                new MovementExecutor(manager, 580);
+
+        var line = Script.readAll("input.txt")[0].trim();
+        var actions = [for (i in line.split(' ')) switch (Std.parseInt(i)) {
+            case 0:
+                Undefined;
+            case 1:
+                TurnLeft;
+            case 2: 
+                TurnRight;
+            case _:
+                Go; 
+        }].filter(function (x) return x != Undefined);
+        Script.print(actions);
+        // var actions = [TurnAround, Go, Go];
+
+        for (action in actions)
+            executor.add(action);
+        executor.execute();
+
+        Brick.display.addLabel('finish', new image.Pixel(0, 0));
     }
 
     public function new(manager:MotorManager, args:ModelArguments):Void {
